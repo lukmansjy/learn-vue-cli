3b0b59d0041f7a5bee6feb41f56972270dc06d91
lukmansjy <lukman.rocks@live.com>
2020-04-28 19:57:54 +0700

    Add Post

--- src/components/post/Add.vue
+++ src/components/post/Add.vue
@@ -1,5 +1,51 @@
 <template>
     <div>
-        <p>Add Page</p>
+        <form @submit.prevent>
+            <div class="form-group">
+                <label for="title">Title</label>
+                <input type="text" id="title" class="form-control" v-model="post.title">
+            </div>
+            <div class="form-group">
+                <label for="desc">Description</label>
+                <textarea id="desc" cols="10" rows="4" class="form-control" v-model="post.description"></textarea>
+            </div>
+            <button class="btn btn-success" @click="postSubmit()">Submit</button>
+        </form>
     </div>
 </template>
+
+<script>
+import PostService from '../../services/PostService'
+export default {
+    name: "post-add",
+    data() {
+        return {
+            post: {
+                id: null,
+                title: "",
+                description: "",
+                published: false
+            }
+        }
+    },
+
+    methods: {
+        postSubmit(){
+            let data = {
+                title: this.post.title,
+                description: this.post.description
+            }
+
+            PostService.create(data)
+                .then(result => {
+                    this.post.id = result.data.id
+                    console.log(result)
+                    this.$router.push({ name: 'posts'})
+                }).catch( err => {
+                    console.log(err)
+                })
+            
+        }
+    }
+}
+</script>
